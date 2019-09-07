40365f08218a2370daa06708613a50584f541c9d
Calvin Hu <calvinhu73@gmail.com>
2019-09-07 16:30:17 -0400

    deleting comment

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from method import *
 from get_events import main
 
 events = main()
-#events = [datetime(2019, 9, 7, 13, 43, 10, 0)] #replace with events = main() when api is set up
 #number = get_phone_number()
 for event in events:
     wait_until_message(event_start_to_datetime(event))
