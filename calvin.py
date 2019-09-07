41fea37a0fc6df9b7847c262fcc438b121939ed0
Calvin Hu <calvinhu73@gmail.com>
2019-09-07 09:29:49 -0400

    Update calvin.py
    added a method to wait for a until 10 minutes before event to send message

--- calvin.py
+++ calvin.py
@@ -1,2 +1,12 @@
-print("asd"
-)
+import datetime
+import time
+from datetime import timedelta
+
+def wait_to_message(event):
+    currentTimeDifference = event - datetime.datetime.now()
+    timeUntilMessage = currentTimeDifference - timedelta(minutes = 10)
+    time.sleep(timeUntilMessage)
+    send_message()
+
+def send_message(): #implement this method
+    NotImplemented
