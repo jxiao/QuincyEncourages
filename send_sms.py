6d64b2c069f12944250808f3cc75537b17b0aa32
Calvin Hu <calvinhu73@gmail.com>
2019-09-07 11:00:41 -0400

    fixing problem

--- send_sms.py
+++ send_sms.py
@@ -4,32 +4,13 @@ from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 account_sid = 'ACee3a49b44290955782cc35f8b5e7259a'
-<<<<<<< HEAD
-auth_token = 'f728fd90ae2e5a83d9b05bc0919b1122'
-=======
-auth_token = 'authtoken'
->>>>>>> e8b8b2a216fcf4981b1b52e30372989ffc6f67d1
+auth_token = 'ask calvin for this'
 client = Client(account_sid, auth_token)
 
-# message = client.messages \
-#     .create(
-#          body='This is the ship that made the Kessel Run in fourteen parsecs?',
-#          from_='15012616860',
-#          to='16466411038'
-#      )
-
-# print(message.sid)
-
-def send_message(message, from_number, to_number): #implement this method
+def send_message(message, from_number, to_number): #ask calvin for from number
     message = client.messages \
     .create(
          body= message,
          from_= str(from_number),
          to= str(to_number)
      )
-
-<<<<<<< HEAD
-send_message("asd", 15012616860, 16466411038)
-=======
-print(message.sid)
->>>>>>> e8b8b2a216fcf4981b1b52e30372989ffc6f67d1
