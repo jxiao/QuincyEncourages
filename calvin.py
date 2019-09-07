d8d91167d00d71168b3548e70e34192ddbcd3734
Calvin Hu <calvinhu73@gmail.com>
2019-09-07 10:52:57 -0400

    updated send_sms.py
    made send_message method

--- calvin.py
+++ calvin.py
@@ -6,7 +6,5 @@ def wait_and_message(event):
     currentTimeDifference = event - datetime.datetime.now()
     timeUntilMessage = currentTimeDifference - timedelta(minutes = 10)
     time.sleep(timeUntilMessage)
-    send_message()
 
-def send_message(): #implement this method
-    NotImplemented
+print("a")
--- send_sms.py
+++ send_sms.py
@@ -4,14 +4,24 @@ from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 account_sid = 'ACee3a49b44290955782cc35f8b5e7259a'
-auth_token = 'fe7e9e713974c4adf0490ea59761889c'
+auth_token = 'f728fd90ae2e5a83d9b05bc0919b1122'
 client = Client(account_sid, auth_token)
 
-message = client.messages \
+# message = client.messages \
+#     .create(
+#          body='This is the ship that made the Kessel Run in fourteen parsecs?',
+#          from_='15012616860',
+#          to='16466411038'
+#      )
+
+# print(message.sid)
+
+def send_message(message, from_number, to_number): #implement this method
+    message = client.messages \
     .create(
-         body='This is the ship that made the Kessel Run in fourteen parsecs?',
-         from_='<calvin has the number',
-         to='enter number here'
+         body= message,
+         from_= str(from_number),
+         to= str(to_number)
      )
 
-print(message.sid)
+send_message("asd", 15012616860, 16466411038)
